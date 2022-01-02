5dc3eaf4bbb8892d0d2c64ef3fc5cd9f3d058a73
Eduard Tokarev <etdevios@gmail.com>
2022-01-03 00:24:22 +0300

    Initial Commit

---
+++ Project 7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 7
+//
+//  Created by Eduard Tokarev on 03.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
